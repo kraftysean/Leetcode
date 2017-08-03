d1ab8a24340d1cd33b23e92d00e4c2f4dc3dc434
kraftysean <kraftysm@gmail.com>
2017-08-03 23:17:54 +0100

    Apply DP solution for problem #139

---
+++ medium/P139_WordBreak.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class P139_WordBreak {
+
+  public boolean wordBreak(String s, List<String> wordDict) {
+    if(s.isEmpty())
+      return true;
+
+    
+    return false;
+  }
+}
---
+++ medium/test/P139_WordBreakTest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class P139_WordBreakTest {
+
+  @Test
+  public void testPositive() {
+    P139_WordBreak wb = new P139_WordBreak();
+    assertTrue(wb.wordBreak("leetcode", Arrays.asList("leet", "code")));
+    assertTrue(wb.wordBreak("cars", Arrays.asList("car", "ca", "rs")));
+  }
+
+  @Test
+  public void testNegative() {
+    P139_WordBreak wb = new P139_WordBreak();
+    assertFalse(wb.wordBreak("", Arrays.asList("leet", "code")));
+    assertFalse(wb.wordBreak("leet code", Arrays.asList("leet", "code")));
+    assertFalse(wb.wordBreak("carjacking", Arrays.asList("car", "cars", "jack", "king")));
+  }
+}
